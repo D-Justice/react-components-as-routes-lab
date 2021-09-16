4ab610d8411ad60b748ad2f161eaf348374d436f
Damian <justice.damian96@gmail.com>
2021-09-16 11:48:22 +1000

    Assignment Complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => {
+          return <div>
+            <h1>{movie.title}</h1>
+            <small>Time: {movie.time}</small>
+            <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        })}
     </div>
   );
 };
